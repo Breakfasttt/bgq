737ebac138a481103cadc8bf09c9d0f3613b6d11
Breakyt <jeaneaut@gmail.com>
2018-06-11 16:51:57 +0200

    * fix for android version

--- project/src/misc/name/LayerName.hx
+++ project/src/misc/name/LayerName.hx
@@ -9,14 +9,14 @@ class LayerName
 
 	public static var back : String  = "backLayer";
 	public static var screens : String  = "screensLayer";
-	public static var ath : String  = "athLayer";
+	public static var hud : String  = "hudLayer";
 	public static var popup : String = "popupLayer";
 	public static var front : String = "frontLayer";
 	public static var debug : String = "debugLayer";
 	
 	public static var ordonedLayerNames : Array<String> = [	LayerName.back, 
 															LayerName.screens, 
-															LayerName.ath, 
+															LayerName.hud, 
 															LayerName.popup, 
 															LayerName.front, 
 															LayerName.debug ];
